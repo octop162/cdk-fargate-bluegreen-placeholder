9b916177a877e0f7fab352814944c9ba5cdc8657
octop162 <octop162@icloud.com>
2022-02-19 13:11:05 +0900

    Add Docker Image

---
+++ image/app/app.py
@@ -0,0 +1,18 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def index():
+    return "<h3>It Works.</h3>"
+
+
+@app.route("/about")
+def about():
+    return "<h3>About.</h3>"
+
+
+@app.route("/health")
+def health():
+    return "<h3>Audley.</h3>"
---
+++ image/app/entrypoint.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+uwsgi --ini /app/uwsgi.ini
+
+
